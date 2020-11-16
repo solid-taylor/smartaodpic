edfa518237edc710dc23ac4d73f3342e427df8e8
solid-taylor <64595121+solid-taylor@users.noreply.github.com>
2020-11-16 16:14:29 +0100

    init

---
+++ extract.py
@@ -0,0 +1,51 @@
+import PyPDF2
+from os import listdir
+from os.path import isfile, join
+from os import walk
+
+
+mypath = 'P:\\Temp\\Tapio_Develop\\receipt\\'
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+for f in onlyfiles:
+    extractAttachments(mypath, f)
+
+''' f = []
+for (dirpath, dirnames, filenames) in walk(mypath):
+    #f.extend(filenames)
+    extractAttachments(mypath, filenames)
+    break '''
+
+
+
+def getAttachments(reader):
+    """
+    Retrieves the file attachments of the PDF as a dictionary of file names
+    and the file data as a bytestring.
+
+    :return: dictionary of filenames and bytestrings
+    """
+    catalog = reader.trailer["/Root"]
+    fileNames = catalog['/Names']['/EmbeddedFiles']['/Names']
+    attachments = {}
+    for f in fileNames:
+        if isinstance(f, str):
+            name = f
+            dataIndex = fileNames.index(f) + 1
+            fDict = fileNames[dataIndex].getObject()
+            fData = fDict['/EF']['/F'].getData()
+            attachments[name] = fData
+    return attachments
+
+
+def extractAttachments(path, filename):
+    handler = open(path + filename, 'rb')
+    reader = PyPDF2.PdfFileReader(handler)
+    dictionary = getAttachments(reader)
+    print(dictionary)
+    for fName, fData in dictionary.items():
+        with open(path + filename + '_' + fName, 'wb') as outfile:
+            outfile.write(fData)
+
+
